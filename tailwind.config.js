e56394fe2dd671330a3a8bb939bac43556ebbc93
Mariam Mohamed Ibrahim <mariamm.ibrahim20@gmail.com>
2024-01-16 12:32:20 +0200

    change paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,8 @@ module.exports = {
         'secondary':"#333333"
       },
       backgroundImage: {
-        'home-background':"url(../dist/img/bg.jpg)",
-        'dashboard-background' : "url(../dist/img/bg2.jpg)"
+        'home-background':"url(dist/img/bg.jpg)",
+        'dashboard-background' : "url(dist/img/bg2.jpg)"
       },
       fontSize: {
         header: ['60px', '5.75rem'],
